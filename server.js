7609650b3573bf61c5df5663f6580245da266c1b
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-09 15:16:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,28 +5,28 @@ app.use(express.json());
 
 // Example: Forward request to User Service
 app.post('/users/register', (req, res) => {
-  axios.post('http://localhost:5000/api/users/register', req.body)
+  axios.post('http://localhost:5000/api/users/register', req.body, { timeout: 10000 })
     .then(response => res.json(response.data))
     .catch(err => res.status(err.response?.status || 500).json({ error: err.message }));
 });
 
 // Example: Fetch all movies
 app.get('/movies', (req, res) => {
-  axios.get('http://localhost:5001/api/movies')
+  axios.get('http://localhost:5001/api/movies', { timeout: 10000 })
     .then(response => res.json(response.data))
     .catch(err => res.status(500).json({ error: err.message }));
 });
 
 // Example: Get recommendations
 app.get('/recommendations/:userId', (req, res) => {
-  axios.get(`http://localhost:5002/api/recommendations/${req.params.userId}`)
+  axios.get(`http://localhost:5002/api/recommendations/${req.params.userId}`, { timeout: 10000 })
     .then(response => res.json(response.data))
     .catch(err => res.status(500).json({ error: err.message }));
 });
 
 // Example: Get reviews for a movie
 app.get('/reviews/:movieId', (req, res) => {
-  axios.get(`http://localhost:5003/api/reviews/${req.params.movieId}`)
+  axios.get(`http://localhost:5003/api/reviews/${req.params.movieId}`, { timeout: 10000 })
     .then(response => res.json(response.data))
     .catch(err => res.status(500).json({ error: err.message }));
 });
